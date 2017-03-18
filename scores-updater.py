d432ea332ddbd0467737fd5bfe8cd48489e6f3e9
Uladzislau Paulovich <selatnick@gmail.com>
2017-03-19 00:05:53 +0300

    Improve scorer

--- scores-updater.py
+++ scores-updater.py
@@ -2,6 +2,7 @@
 
 import github3
 from datetime import datetime
+from datetime import date
 from dateutil import tz
 import re
 import httplib2
@@ -28,6 +29,7 @@ except ImportError:
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Leetcode marathon status updater'
+MARATHON_START_DATE = date(2017, 3, 18)
 
 
 def get_credentials():
@@ -75,12 +77,29 @@ def main():
     today = datetime.utcnow().date()
     start = datetime(today.year, today.month, today.day, tzinfo=tz.tzutc())
     g = github3.login(local_secrets.USER_NAME, local_secrets.GITHUB_ACCESS_TOKEN)
+    solved_today_tasks = []
+    print('Participant scores:')
     for i in range(len(values[0])):
         r = g.repository(values[0][i], values[1][i])
         solved_today = set()
         for c in r.commits(since=start):
             solved_today.update(parse_solved_tasks(c.message))
+        solved_today_tasks.append(len(solved_today))
         print(values[0][i], len(solved_today))
+
+    print('Updating the table...', end='')
+    rowid = (today - MARATHON_START_DATE).days + 8
+    rangeTodayTasks = 'Days!B{rowid}:I{rowid}'.format(rowid=rowid)
+    values = [
+        solved_today_tasks
+    ]
+    writeBody = {
+        'values': values
+    }
+    result = service.spreadsheets().values().update(
+        spreadsheetId=local_secrets.SPREADSHEET_ID, range=rangeTodayTasks,
+        valueInputOption='RAW', body=writeBody).execute()
+    print('ok!')
 
 
 if __name__ == '__main__':
